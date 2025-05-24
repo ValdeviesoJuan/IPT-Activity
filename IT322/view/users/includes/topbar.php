18061ed1143ca2e58a1730f1d6fb3b64c6ab88bc
Juan Carlos <160289942+ValdeviesoJuan@users.noreply.github.com>
2025-05-24 20:39:22 +0800

    added profile users

--- IT322/view/users/includes/topbar.php
+++ IT322/view/users/includes/topbar.php
@@ -86,7 +86,11 @@ $notificationCount = $result->num_rows;
       <li class="nav-item dropdown pe-3">
 
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <img src="../../assets/img/profileImage.png" alt="Profile" class="rounded-circle">
+          <img src="<?php echo !empty($_SESSION['authUser']['profilePicture']) ? $_SESSION['authUser']['profilePicture'] : '../../assets/img/profileImage.png'; ?>" 
+          alt="<?php echo htmlspecialchars($fullName); ?>'s Profile Picture"
+          class="rounded-circle me-2"
+          style="width: 35px; height: 35px; object-fit: cover; border: solid 1px white"
+        >
           <span class="d-none d-md-block dropdown-toggle ps-2" style="color: white;"><?php echo htmlspecialchars($fullName); ?></span>
         </a><!-- End Profile Iamge Icon -->
 
@@ -100,29 +104,9 @@ $notificationCount = $result->num_rows;
           </li>
 
           <li>
-            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+            <a class="dropdown-item d-flex align-items-center" href="../userProfile.php">
               <i class="bi bi-person"></i>
               <span>My Profile</span>
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li>
-            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-              <i class="bi bi-gear"></i>
-              <span>Account Settings</span>
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li>
-            <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-              <i class="bi bi-question-circle"></i>
-              <span>Need Help?</span>
             </a>
           </li>
           <li>
@@ -136,11 +120,11 @@ $notificationCount = $result->num_rows;
             </a>
           </li>
 
-        </ul><!-- End Profile Dropdown Items -->
-      </li><!-- End Profile Nav -->
+        </ul>
+      </li>
 
     </ul>
-  </nav><!-- End Icons Navigation -->
+  </nav>
 
   <style>
     .search-bar .search-form .main-search-input{
